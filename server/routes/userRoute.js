47f19d2550327a871869b03da16cb33d17328597
Sandeep-morya <saabmaurya@gmail.com>
2023-03-03 12:37:38 +0530

    opted

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,10 +1,18 @@
 ﻿const express = require("express");
 const User = require("../models/userModel");
+const asyncHandler = require("express-async-handler");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
 	res.send("hi");
 });
+
+router.post(
+	"/register",
+	asyncHandler(async (req, res) => {
+		res.send(req.body);
+	}),
+);
 
 module.exports = router;
