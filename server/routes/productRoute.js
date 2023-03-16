efd7fbe4357ab917797c981f5304c208f94c972c
Sandeep-morya <saabmaurya@gmail.com>
2023-03-16 19:33:09 +0530

    routes added for filter

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -9,7 +9,52 @@ const router = express.Router();
 /* Middlewares */
 router.use(genIdMiddleware);
 
-/* get all Products*/
+/* match by title & brand */
+router.get(
+	"/search",
+	asyncHandler(async (req, res) => {
+		const query = { $regex: req.query.q, $options: "i" };
+		const products = await Product.find({
+			$or: [{ title: query }, { brand: query }],
+		});
+		res.send(products);
+	}),
+);
+
+/* Categroy match */
+router.get(
+	"/category",
+	asyncHandler(async (req, res) => {
+		const query = { $regex: req.query.q, $options: "i" };
+		const products = await Product.find({
+			tags: { $elemMatch: query },
+		});
+		res.send(products);
+	}),
+);
+
+/* filter based on price, rating, discount */
+router.get(
+	"/filter",
+	asyncHandler(async (req, res) => {
+		const base = req.query.base || "price";
+		const min = req.query.min || 0;
+		const max = req.query.max || Infinity;
+		const order =
+			req.query.order === "asc"
+				? { [base]: "1" }
+				: req.query.order === "dsc"
+				? { [base]: "-1" }
+				: 0;
+
+		const products = await Product.find()
+			.where(base)
+			.gte(min)
+			.lte(max)
+			.sort(order);
+		res.send({ total: products.length, products });
+	}),
+);
 
 router.get(
 	"/all",
@@ -29,6 +74,7 @@ router.get(
 		res.send(product);
 	}),
 );
+
 router.get(
 	"/seller/:id",
 	asyncHandler(async (req, res) => {
@@ -76,6 +122,43 @@ router.delete(
 			seller: req.body.seller,
 		});
 		res.send(data);
+	}),
+);
+
+/* ******************* Extras  higest time taking */
+
+// /* match category & sizes */
+
+router.get(
+	"/match",
+	asyncHandler(async (req, res) => {
+		const key = Object.keys(req.query)[0];
+
+		const value = Object.values(req.query)[0];
+		const query = { $regex: value, $options: "i" };
+		const products = await Product.find({
+			[key]: { $elemMatch: query },
+		});
+		res.send(products);
+	}),
+);
+
+/* Hard match  */
+router.get(
+	"/",
+	asyncHandler(async (req, res) => {
+		const query = { $regex: req.query.q, $options: "i" };
+		const products = await Product.find({
+			$or: [
+				{ title: query },
+				{ brand: query },
+				{ description: query },
+				{ is_for: query },
+				{ for_gender: query },
+				{ for_age: query },
+			],
+		});
+		res.send(products);
 	}),
 );
 
